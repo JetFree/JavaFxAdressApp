3d6d2a10beb33b43788ec39132acb5c2427e247d
JetFree <l7egnf@gmail.com>
2019-05-10 00:13:19 +0300

    Added custom styles Added icon for application

--- src/ch/makery/address/MainApp.java
+++ src/ch/makery/address/MainApp.java
@@ -8,6 +8,7 @@ import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.BorderPane;
 import javafx.stage.Modality;
@@ -25,6 +26,7 @@ public class MainApp extends Application {
     public void start(Stage primaryStage) {
         this.primaryStage = primaryStage;
         this.primaryStage.setTitle("AdressApp");
+        this.primaryStage.getIcons().add(new Image("images/iconfinder_Address_Book_86957.png"));
         initRootLayout();
         showPersonOverview();
     }
